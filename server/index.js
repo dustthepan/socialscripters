9a495f949ddd81c0cc987e6d11ed9f112c0c3c13
dustthepan <dustintrandev@gmail.com>
2020-07-23 15:47:19 -0700

    initial committ

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+//const path = require('path');
+
+const app = express();
+
+app.get('/',(req,res) => {
+    res.send('API runnning')
+})
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log('Server running on 5000'))
+
+
+
+
