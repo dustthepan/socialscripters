ee98aa7971d2efa1487440c522688fb171dcf548
dustthepan <dustintrandev@gmail.com>
2020-07-23 23:21:50 -0700

    fix deprecation

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ const db = config.get('mongoURI');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(db, {useNewUrlParser: true});
+        await mongoose.connect(db, {useUnifiedToplogy: true});
         
         console.log('MongoDB connected')
     } catch(err) {
